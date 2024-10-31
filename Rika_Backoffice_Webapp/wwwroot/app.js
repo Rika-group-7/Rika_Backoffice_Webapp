37cbf14ab30ee5b89ce2a73f9d5282a3a9a78e34
najib ebrahimi <najib.ebrahimi144@gmail.com>
2024-10-31 17:57:31 +0100

    första utkast

---
+++ Rika_Backoffice_Webapp/wwwroot/app.js
@@ -0,0 +1,7 @@
+﻿function navigateTo(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+        alert(`Navigerar till: ${section.querySelector("h2").innerText}`);
+    }
+}
