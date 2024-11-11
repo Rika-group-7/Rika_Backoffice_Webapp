43aa651b4d0db7577f8783f8b2de11eca8d503f1
hugospangberg <hugospangberg@HUGOSPNGBER517B>
2024-11-11 22:11:03 +0100

    Enhance order details view and clean up code
    Updated UserManagementDetails.razor to display additional order details, including product information and total amount, in a tabular format. Removed unused variables from the @code section. Added TotalAmount property to OrderModel.cs to store the total order amount.

--- Rika_Backoffice_Webapp/Models/OrderModel.cs
+++ Rika_Backoffice_Webapp/Models/OrderModel.cs
@@ -6,6 +6,7 @@
         public string CustomerEmail { get; set; } = null!;
         public int OrderNumber { get; set; }
         public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
         public List<OrderItemDto> OrderItems { get; set; } = new List<OrderItemDto>();
     }
 
