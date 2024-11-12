5858571b8eb4e9fcb548b10bfb4362f30ba955ab
SebastianWirstrom <143399640+SebastianWirstrom@users.noreply.github.com>
2024-11-12 15:06:19 +0100

    added form validation to CreateProduct form 20241112

--- Rika_Backoffice_Webapp/Models/ProductCreateRequest.cs
+++ Rika_Backoffice_Webapp/Models/ProductCreateRequest.cs
@@ -1,19 +1,45 @@
 ﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace Rika_Backoffice_Webapp.Models;
 
 public class ProductCreateRequest
 {
     public string? Id { get; set; }
+
+    [Required(ErrorMessage = "Title is mandatory")]
+    [MinLength(2)]
     public string? Title { get; set; }
+
+    [Required(ErrorMessage = "Brand is mandatory")]
+    [MinLength(2)]
     public string? Brand { get; set; }
+
+    [Required(ErrorMessage = "Size is mandatory")]
+    [MinLength(2)]
     public string? Size { get; set; }
+
+    [Required(ErrorMessage = "Color is mandatory")]
+    [MinLength(2)]
     public string? Color { get; set; }
-    public decimal Price { get; set; }
+
+    [Required(ErrorMessage = "Price is mandatory")]
+    public decimal? Price { get; set; }
+
+    [Required(ErrorMessage = "Description is mandatory")]
+    [MinLength(2)]
     public string? Description { get; set; }
+
     public bool StockStatus { get; set; }
+
+    [Required(ErrorMessage = "SKU is mandatory")]
+    [MinLength(2)]
     public string? SKU { get; set; }
-    public decimal Ratings { get; set; }
+
+    [Required(ErrorMessage = "Rating is mandatory")]
+    public decimal? Ratings { get; set; }
+
+    [Required(ErrorMessage = "Product image is mandatory")]
     public string? ProductImage { get; set; }
 
     public List<CategoryCreateRequest>? Categories { get; set; } = new();
