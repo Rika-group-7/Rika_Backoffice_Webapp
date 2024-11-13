7cd8fb584fb06364754ebeaafe190688b014b68d
SebastianWirstrom <143399640+SebastianWirstrom@users.noreply.github.com>
2024-11-13 12:00:45 +0100

    Product update added validation to model and form

--- Rika_Backoffice_Webapp/Models/ProductUpdateRequest.cs
+++ Rika_Backoffice_Webapp/Models/ProductUpdateRequest.cs
@@ -1,17 +1,43 @@
-﻿namespace Rika_Backoffice_Webapp.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Rika_Backoffice_Webapp.Models;
 
 public class ProductUpdateRequest
 {
     public string? Id { get; set; }
+
+    [Required(ErrorMessage = "Title is mandatory")]
+    [MinLength(2)]
     public string? Title { get; set; }
+
+    [Required(ErrorMessage = "Brand is mandatory")]
+    [MinLength(2)]
     public string? Brand { get; set; }
+
+    [Required(ErrorMessage = "Size is mandatory")]
     public string? Size { get; set; }
+
+    [Required(ErrorMessage = "Color is mandatory")]
+    [MinLength(2)]
     public string? Color { get; set; }
-    public decimal Price { get; set; }
+
+    [Required(ErrorMessage = "Price is mandatory")]
+    public decimal? Price { get; set; }
+
+    [Required(ErrorMessage = "Description is mandatory")]
+    [MinLength(2)]
     public string? Description { get; set; }
+
     public bool StockStatus { get; set; }
+
+    [Required(ErrorMessage = "SKU is mandatory")]
+    [MinLength(2)]
     public string? SKU { get; set; }
-    public decimal Ratings { get; set; }
+
+    [Required(ErrorMessage = "Rating is mandatory")]
+    public decimal? Ratings { get; set; }
+
+    [Required(ErrorMessage = "Product image is mandatory")]
     public string? ProductImage { get; set; }
 
     public List<CategoryUpdateRequest>? Categories { get; set; } = [];
